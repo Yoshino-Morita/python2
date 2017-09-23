bdb8f38c679629e9e43a80127e900e97a535d6fa
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-09-23 20:15:16 +0900

    Study

--- hello.php
+++ hello.php
@@ -1,14 +1,23 @@
 <?php
 
+$post = array();
+
+echo "レビュー数:0";
+echo "[0]レビューを書く";
+echo "[1]レビューを読む";
+echo "[2]アプリを終了する";
+$input = trim(fgets(STDIN));
+
 echo "ジャンルを入力してください\n";
-$genre = trim(fgets(STDIN));
+$post['genre'] = trim(fgets(STDIN));
 echo "タイトルを入力してください\n";
-$title = trim(fgets(STDIN));
+$post['title'] = trim(fgets(STDIN));
 echo "感想を入力してください\n";
-$review = trim(fgets(STDIN));
+$post['review'] = trim(fgets(STDIN));
 $line = "----------------";
 
-echo "ジャンル：{$genre}\n{$line}\n";
-echo "タイトル:{$title}\n{$line}\n";
-echo "感想：\n{$review}\n{$line}\n";
+echo "ジャンル：{$post['genre']}\n{$line}\n";
+echo "タイトル:{$post['title']}\n{$line}\n";
+echo "感想：\n{$post['review']}\n{$line}\n";
+
 ?>
