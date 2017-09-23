35c04cae80264cc1e4a1e9838f2fba09a2569aad
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-09-23 19:29:17 +0900

    Php

--- hello.php
+++ hello.php
@@ -1,5 +1,16 @@
 <?php
 
-echo "Hello World",PHP_EOL;
+$genre = "映画\n";
+$title = "時をかける少女\n";
+$review = "あああ\n";
+$line = "----------------";
+
+echo "ジャンル：".$genre;
+echo $line;
+echo "タイトル:".$title;
+echo $line;
+echo "感想：";
+echo $review;
+echo $line;
 
 ?>
