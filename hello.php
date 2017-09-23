98a7b09f0f9252e942ec30b11249d5a196141173
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-09-23 19:49:21 +0900

    Study

--- hello.php
+++ hello.php
@@ -1,8 +1,11 @@
 <?php
 
-$genre = "映画";
-$title = "時をかける少女";
-$review = "あああ";
+echo "ジャンルを入力してください\n";
+$genre = trim(fgets(STDIN));
+echo "タイトルを入力してください\n";
+$title = trim(fgets(STDIN));
+echo "感想を入力してください\n";
+$review = trim(fgets(STDIN));
 $line = "----------------";
 
 echo "ジャンル：{$genre}\n{$line}\n";
